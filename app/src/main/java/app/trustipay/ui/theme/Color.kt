2e537fb51f5e544a24e5530ef59f631275453c36
Chinthana-Weligamage <chinthanaweligamage1@gmail.com>
2026-04-26 11:35:00 +0530

    initial commit

---
+++ app/src/main/java/app/trustipay/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package app.trustipay.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val TrustiPayPrimary = Color(0xFF124170) // Dark Blue
+val TrustiPaySecondary = Color(0xFF26667F) // Teal
+val TrustiPayTertiary = Color(0xFF67C090) // Medium Green
+val TrustiPayBackground = Color(0xFFDDF4E7) // Light Mint
+
+val DarkBlue = Color(0xFF124170)
+val Teal = Color(0xFF26667F)
+val Green = Color(0xFF67C090)
+val LightMint = Color(0xFFDDF4E7)
